ec80e1ec808f53b72d369f350b53015eba02d89e
N1K0232 <ns.nicolasilvestri@libero.it>
2023-10-23 17:32:08 +0200

    Add null check for path argument

--- src/StorageProviders.Abstractions/StorageCache.cs
+++ src/StorageProviders.Abstractions/StorageCache.cs
@@ -13,15 +13,18 @@ internal class StorageCache : IStorageCache
 
     public Task DeleteAsync(string path, CancellationToken cancellationToken = default)
     {
+        ArgumentException.ThrowIfNullOrEmpty(path, nameof(path));
         cancellationToken.ThrowIfCancellationRequested();
-        cache.Remove(path);
 
+        cache.Remove(path);
         return Task.CompletedTask;
     }
 
     public Task<Stream?> ReadAsync(string path, CancellationToken cancellationToken = default)
     {
+        ArgumentException.ThrowIfNullOrEmpty(path, nameof(path));
         cancellationToken.ThrowIfCancellationRequested();
+
         if (cache.TryGetValue(path, out Stream? stream))
         {
             return Task.FromResult(stream);
@@ -32,9 +35,10 @@ internal class StorageCache : IStorageCache
 
     public Task SetAsync(string path, Stream stream, TimeSpan expiration, CancellationToken cancellationToken = default)
     {
+        ArgumentException.ThrowIfNullOrEmpty(path, nameof(path));
         cancellationToken.ThrowIfCancellationRequested();
-        cache.Set(path, stream, expiration);
 
+        cache.Set(path, stream, expiration);
         return Task.CompletedTask;
     }
 }
