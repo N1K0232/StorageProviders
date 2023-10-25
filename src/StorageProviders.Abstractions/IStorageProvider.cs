09477687705b13e07d65b9719d0b6388084bf513
N1K0232 <ns.nicolasilvestri@libero.it>
2023-10-25 13:03:43 +0200

    Add UploadAsync, EnumerateAsync, ReadAsByteArrayAsync and ReadAsStringAsync to interface

--- src/StorageProviders.Abstractions/IStorageProvider.cs
+++ src/StorageProviders.Abstractions/IStorageProvider.cs
@@ -8,9 +8,17 @@ public interface IStorageProvider : IDisposable
 
     IAsyncEnumerable<string> EnumerateAsync(string? prefix, string[] extensions, CancellationToken cancellationToken = default);
 
+    IAsyncEnumerable<string> EnumerateAsync(string? prefix = null, params string[] extensions);
+
     Task<StorageFileInfo?> GetPropertiesAsync(string path, CancellationToken cancellationToken = default);
 
     Task<Stream?> ReadAsync(string path, CancellationToken cancellationToken = default);
 
+    Task<byte[]?> ReadAsByteArrayAsync(string path, CancellationToken cancellationToken = default);
+
+    Task<string?> ReadAsStringAsync(string path, CancellationToken cancellationToken = default);
+
     Task UploadAsync(string path, Stream stream, bool overwrite = false, CancellationToken cancellationToken = default);
+
+    Task UploadAsync(string path, byte[] content, bool overwrite = false, CancellationToken cancellationToken = default);
 }
